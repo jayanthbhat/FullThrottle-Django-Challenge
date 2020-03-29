ceefd6f5eee24be8b57955565e7869b20d13752e
jayanth.ganesh <jayanthbhat93@gmail.com>
2020-03-29 13:30:30 +0530

    added home page

--- fullthrottle_app/urls.py
+++ fullthrottle_app/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
 from fullthrottle_app.api import UserListAPIView
-from fullthrottle_app.views import command_upload
+from fullthrottle_app.views import command_upload,home
 
 # url for fullthrottle_app
 urlpatterns = [
+    path('',home,name="home"),
     path('api/list/',UserListAPIView.as_view(), name="list_api"),
-    path('api/throttle-command/',command_upload,name="command"),
+    path('throttle-command/',command_upload,name="command"),
 ]
--- fullthrottle_app/views.py
+++ fullthrottle_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponseBadRequest, HttpResponse
 from django.core.management import call_command
 
+def home(request):
+    return render(request,'home.html')
+
 def command_upload(request):
     if request.method == "POST":
         username = request.POST['username']
